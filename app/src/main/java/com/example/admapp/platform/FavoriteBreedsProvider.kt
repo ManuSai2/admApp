3edde962e532f49e455ec9f1ddfbd0ac71eda456
Ximena Gonzalez <ximee.gonzalez91@gmail.com>
2026-05-26 23:27:30 -0300

    Agrego configuracion Fastlane

---
+++ app/src/main/java/com/example/admapp/platform/FavoriteBreedsProvider.kt
@@ -0,0 +1,48 @@
+package com.example.admapp.platform
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.database.Cursor
+import android.net.Uri
+
+class FavoriteBreedsProvider : ContentProvider() {
+
+    override fun onCreate(): Boolean {
+        return true
+    }
+
+    override fun query(
+        uri: Uri,
+        projection: Array<out String>?,
+        selection: String?,
+        selectionArgs: Array<out String>?,
+        sortOrder: String?
+    ): Cursor? {
+        return null
+    }
+
+    override fun getType(uri: Uri): String? {
+        return "vnd.android.cursor.dir/vnd.com.example.admapp.favoritebreeds"
+    }
+
+    override fun insert(uri: Uri, values: ContentValues?): Uri? {
+        return null
+    }
+
+    override fun delete(
+        uri: Uri,
+        selection: String?,
+        selectionArgs: Array<out String>?
+    ): Int {
+        return 0
+    }
+
+    override fun update(
+        uri: Uri,
+        values: ContentValues?,
+        selection: String?,
+        selectionArgs: Array<out String>?
+    ): Int {
+        return 0
+    }
+}
